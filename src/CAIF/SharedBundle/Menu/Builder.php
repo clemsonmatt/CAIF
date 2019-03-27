8eda194e2e45d24d3446625690671a986d3be543
Matt Ellis <mellis@ncees.org>
2019-03-26 22:41:24 -0400

    Update nav and make more moble friendly #12

--- src/CAIF/SharedBundle/Menu/Builder.php
+++ src/CAIF/SharedBundle/Menu/Builder.php
@@ -12,48 +12,47 @@ class Builder extends ContainerAware
         $menu = $factory->createItem('root');
         $menu->setChildrenAttribute('class', 'nav navbar-nav');
 
-        $menu->addChild('Home', ['class' => 'dropdown-toggle'])
+        $menu->addChild('About', ['class' => 'dropdown-toggle'])
             ->setAttribute('class', 'dropdown')
             ->setChildrenAttribute('class', 'dropdown-menu');
-        $menu['Home']->addChild('Home Page', ['route' => 'caif_shared_index']);
-        $menu['Home']->addChild('What Folks Are Saying', ['route' => 'caif_shared_quotes']);
-        // $menu['Home']->addChild('Officers', ['route' => 'caif_shared_officers']);
-        $menu['Home']->addChild('Photo Gallery', ['route' => 'caif_shared_gallery']);
-        // $menu['Home']->addChild('Newsletters', ['uri' => '#']);
+        $menu['About']->addChild('Who We Are', ['route' => 'caif_shared_quotes']);
+        $menu['About']->addChild('Our Mission', ['route' => 'caif_shared_quotes']);
+        $menu['About']->addChild('Testimonials', ['route' => 'caif_shared_quotes']);
+        $menu['About']->addChild('Photos', ['route' => 'caif_shared_gallery']);
+        $menu['About']->addChild('Board Members', ['route' => 'caif_shared_officers']);
 
-        $menu->addChild('Students', ['class' => 'dropdown-toggle'])
+        $menu->addChild('Host Home Program', ['class' => 'dropdown-toggle'])
             ->setAttribute('class', 'dropdown')
             ->setChildrenAttribute('class', 'dropdown-menu');
-        $menu['Students']->addChild('Form For New Students', ['route' => 'caif_shared_student_form']);
-        $menu['Students']->addChild('Student Guidelines', ['route' => 'caif_shared_student_guidelines']);
-        // $menu['Students']->addChild('Life In the U.S.', ['uri' => '#']);
+        $menu['Host Home Program']->addChild('About The Program', ['route' => 'caif_shared_host_about']);
+        $menu['Host Home Program']->addChild('Student Guidelines', ['route' => 'caif_shared_student_guidelines']);
+        $menu['Host Home Program']->addChild('Form For New Students', ['route' => 'caif_shared_student_form']);
+        $menu['Host Home Program']->addChild('Intro to Hosting Students', ['route' => 'caif_shared_host_activity']);
+        $menu['Host Home Program']->addChild('Host Guidelines', ['route' => 'caif_shared_host_guidelines']);
+        $menu['Host Home Program']->addChild('Form For New Hosts', ['route' => 'caif_shared_host_form']);
 
-        $menu->addChild('Community Members', ['uri' => '#', 'class' => 'dropdown-toggle'])
+        $menu->addChild('Upcomming Events', ['uri' => '#', 'class' => 'dropdown-toggle'])
             ->setAttribute('class', 'dropdown')
             ->setChildrenAttribute('class', 'dropdown-menu');
-        $menu['Community Members']->addChild('About The Program', ['route' => 'caif_shared_host_about']);
-        $menu['Community Members']->addChild('Form For New Community Members', ['route' => 'caif_shared_host_form']);
-        $menu['Community Members']->addChild('Community Member Guidelines', ['route' => 'caif_shared_host_guidelines']);
-        $menu['Community Members']->addChild('Activity Ideas', ['route' => 'caif_shared_host_activity']);
-        $menu['Community Members']->addChild('Conversation Starters', ['route' => 'caif_shared_host_conversation']);
-        $menu['Community Members']->addChild('What To Serve', ['route' => 'caif_shared_host_serve']);
+        $menu['Upcomming Events']->addChild('Current Events', ['route' => 'caif_shared_event_index']);
+        $menu['Upcomming Events']->addChild('International Club (Bridges Int\'l)', ['route' => 'caif_shared_event_intl_club']);
+        $menu['Upcomming Events']->addChild('International Wives Club', ['route' => 'caif_shared_event_wives_club']);
 
-        $menu->addChild('Events & Services', ['uri' => '#', 'class' => 'dropdown-toggle'])
+        $menu->addChild('Helpful Resources', ['uri' => '#', 'class' => 'dropdown-toggle'])
             ->setAttribute('class', 'dropdown')
             ->setChildrenAttribute('class', 'dropdown-menu');
-        $menu['Events & Services']->addChild('Events', ['route' => 'caif_shared_event_index']);
-        $menu['Events & Services']->addChild('English Classes', ['route' => 'caif_shared_event_english_classes']);
-        $menu['Events & Services']->addChild('International Club (Bridges Int\'l)', ['route' => 'caif_shared_event_intl_club']);
-        $menu['Events & Services']->addChild('International Wives Club', ['route' => 'caif_shared_event_wives_club']);
+        $menu['Helpful Resources']->addChild('Adult Learning Center', ['uri' => 'http://www.pickens.k12.sc.us/alc/pages/3.%20our%20programs/esl.aspx'])->setLinkAttributes(['target' => '_blank']);
+        $menu['Helpful Resources']->addChild('Clemson Community Care', ['uri' => 'http://www.clemsoncommunitycare.org/'])->setLinkAttributes(['target' => '_blank']);
+        $menu['Helpful Resources']->addChild('Buying a Used Car', ['uri' => 'https://docs.google.com/presentation/d/1A047101edGc_aDRClVG3GMki5VeQSBofhLwoXjgR6Eo/edit#slide=id.p4'])->setLinkAttributes(['target' => '_blank']);
+        $menu['Helpful Resources']->addChild('English Classes', ['route' => 'caif_shared_event_english_classes']);
+        $menu['Helpful Resources']->addChild('When In Clemson', ['uri' => 'http://www.xaclemson.com/'])->setLinkAttributes(['target' => '_blank']);
+        $menu['Helpful Resources']->addChild('International Services Clemson', ['uri' => 'http://caif.dev/bundles/caifshared/images/MSA_Flyer.pdf'])->setLinkAttributes(['target' => '_blank']);
 
-        $menu['Events & Services']->addChild('Chi Alpha Student Church', ['uri' => 'http://www.xaclemson.com/'])->setLinkAttributes(['target' => '_blank']);
-        $menu['Events & Services']->addChild('Adult Learning Center', ['uri' => 'http://www.pickens.k12.sc.us/alc/pages/3.%20our%20programs/esl.aspx'])->setLinkAttributes(['target' => '_blank']);
-        $menu['Events & Services']->addChild('Muslim Student Association', ['uri' => 'http://caif.dev/bundles/caifshared/images/MSA_Flyer.pdf'])->setLinkAttributes(['target' => '_blank']);
-        $menu['Events & Services']->addChild('Friends of Internationals', ['uri' => 'https://www.facebook.com/groups/FriendsOfInternationals.Clemson/'])->setLinkAttributes(['target' => '_blank']);
-        $menu['Events & Services']->addChild('Reformed University Fellowship Internationals', ['uri' => 'https://www.facebook.com/pages/RUF-I-Clemson/1489222417964573?fref=ts'])->setLinkAttributes(['target' => '_blank']);
-        $menu['Events & Services']->addChild('Clemson Bridges International', ['uri' => 'https://www.facebook.com/groups/clemsonbridgesinternational/'])->setLinkAttributes(['target' => '_blank']);
-        $menu['Events & Services']->addChild('Clemson Community Care', ['uri' => 'http://www.clemsoncommunitycare.org/'])->setLinkAttributes(['target' => '_blank']);
-        $menu['Events & Services']->addChild('Buying a Used Car', ['uri' => 'https://docs.google.com/presentation/d/1A047101edGc_aDRClVG3GMki5VeQSBofhLwoXjgR6Eo/edit#slide=id.p4'])->setLinkAttributes(['target' => '_blank']);
+        $menu->addChild('Alumni', ['uri' => '#', 'class' => 'dropdown-toggle'])
+            ->setAttribute('class', 'dropdown')
+            ->setChildrenAttribute('class', 'dropdown-menu');
+        $menu['Alumni']->addChild('About', ['uri' => '#']);
+        $menu['Alumni']->addChild('Ways To Give Back', ['uri' => '#']);
 
         return $menu;
     }
