d379a6ea58b55c92abb083ba35bd6b264203f1f3
Matt Ellis <mellis@ncees.org>
2019-04-05 18:48:57 -0400

    Update navigation #12

--- src/CAIF/SharedBundle/Menu/Builder.php
+++ src/CAIF/SharedBundle/Menu/Builder.php
@@ -17,30 +17,29 @@ class Builder extends ContainerAware
             ->setChildrenAttribute('class', 'dropdown-menu');
         $menu['About']->addChild('Who We Are', ['route' => 'caif_shared_about']);
         $menu['About']->addChild('Our Mission', ['route' => 'caif_shared_testimonials']);
+        $menu['About']->addChild('About The Program', ['route' => 'caif_shared_host_about']);
         $menu['About']->addChild('Testimonials', ['route' => 'caif_shared_testimonials']);
-        $menu['About']->addChild('Photos', ['route' => 'caif_shared_gallery']);
         $menu['About']->addChild('Board Members', ['route' => 'caif_shared_officers']);
+        $menu['About']->addChild('Upcoming Events', ['uri' => 'https://www.facebook.com/groups/ClemsonAreaInternationalFriendship/'])->setLinkAttributes(['target' => '_blank']);
 
-        $menu->addChild('Host Home Program', ['class' => 'dropdown-toggle'])
+        $menu->addChild('Host Program', ['class' => 'dropdown-toggle'])
             ->setAttribute('class', 'dropdown')
             ->setChildrenAttribute('class', 'dropdown-menu');
-        $menu['Host Home Program']->addChild('About The Program', ['route' => 'caif_shared_host_about']);
-        $menu['Host Home Program']->addChild('Student Guidelines', ['route' => 'caif_shared_student_guidelines']);
-        $menu['Host Home Program']->addChild('Form For New Students', ['route' => 'caif_shared_student_form']);
-        $menu['Host Home Program']->addChild('Intro to Hosting Students', ['route' => 'caif_shared_host_conversation']);
-        $menu['Host Home Program']->addChild('Host Guidelines', ['route' => 'caif_shared_host_guidelines']);
-        $menu['Host Home Program']->addChild('Form For New Hosts', ['route' => 'caif_shared_host_form']);
+        $menu['Host Program']->addChild('Intro to Hosting Students', ['route' => 'caif_shared_host_conversation']);
+        $menu['Host Program']->addChild('Host Guidelines', ['route' => 'caif_shared_host_guidelines']);
+        $menu['Host Program']->addChild('Form For New Hosts', ['route' => 'caif_shared_host_form']);
 
-        $menu->addChild('Upcoming Events', ['uri' => '#', 'class' => 'dropdown-toggle'])
+        $menu->addChild('Student Program', ['class' => 'dropdown-toggle'])
             ->setAttribute('class', 'dropdown')
             ->setChildrenAttribute('class', 'dropdown-menu');
-        $menu['Upcoming Events']->addChild('Current Events', ['route' => 'caif_shared_event_index']);
-        $menu['Upcoming Events']->addChild('International Club (Bridges Int\'l)', ['route' => 'caif_shared_event_intl_club']);
-        $menu['Upcoming Events']->addChild('International Wives Club', ['route' => 'caif_shared_event_wives_club']);
+        $menu['Student Program']->addChild('Student Guidelines', ['route' => 'caif_shared_student_guidelines']);
+        $menu['Student Program']->addChild('Form For New Students', ['route' => 'caif_shared_student_form']);
 
         $menu->addChild('Helpful Resources', ['uri' => '#', 'class' => 'dropdown-toggle'])
             ->setAttribute('class', 'dropdown')
             ->setChildrenAttribute('class', 'dropdown-menu');
+        $menu['Helpful Resources']->addChild('International Club (Bridges Int\'l)', ['route' => 'caif_shared_event_intl_club']);
+        $menu['Helpful Resources']->addChild('International Wives Club', ['route' => 'caif_shared_event_wives_club']);
         $menu['Helpful Resources']->addChild('Adult Learning Center', ['uri' => 'http://www.pickens.k12.sc.us/alc/pages/3.%20our%20programs/esl.aspx'])->setLinkAttributes(['target' => '_blank']);
         $menu['Helpful Resources']->addChild('Clemson Community Care', ['uri' => 'http://www.clemsoncommunitycare.org/'])->setLinkAttributes(['target' => '_blank']);
         $menu['Helpful Resources']->addChild('Buying a Used Car', ['uri' => 'https://docs.google.com/presentation/d/1A047101edGc_aDRClVG3GMki5VeQSBofhLwoXjgR6Eo/edit#slide=id.p4'])->setLinkAttributes(['target' => '_blank']);
